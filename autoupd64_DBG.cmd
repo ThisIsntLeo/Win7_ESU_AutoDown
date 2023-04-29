78bc257deb9108ed830b529fdce11ddda7348ff2
This Isn't Leo <macoslover@icloud.com>
2023-04-29 13:01:25 +0800

    Add debug

---
+++ autoupd64_DBG.cmd
@@ -0,0 +1,23 @@
+@echo on
+color 4f
+title Update downloader DEBUG
+cls
+cd..
+cd..
+cd..
+cd..
+cd..
+cd Program Files
+cd Mozilla Firefox
+pause
+color 2f
+cls
+firefox.exe http://download.windowsupdate.com/c/msdownload/update/software/secu/2019/03/windows6.1-kb4490628-x64_d3de52d6987f7c8bdc2c015dca69eac96047c76e.msu
+firefox.exe http://download.windowsupdate.com/c/msdownload/update/software/secu/2019/09/windows6.1-kb4474419-v3-x64_b5614c6cea5cb4e198717789633dca16308ef79c.msu
+firefox.exe http://download.windowsupdate.com/c/msdownload/update/software/secu/2020/04/windows6.1-kb4555449-x64_92202202c3dee2f713f67adf6622851b998c6780.msu
+firefox.exe http://download.windowsupdate.com/d/msdownload/update/software/secu/2020/07/windows6.1-kb4575903-x64_b4d5cf045a03034201ff108c2802fa6ac79459a1.msu
+firefox.exe http://download.windowsupdate.com/d/msdownload/update/software/updt/2016/02/windows6.1-kb3138612-x64_f7b1de8ea7cf8faf57b0138c4068d2e899e2b266.msu
+color 0f
+cls
+pause
+exit
---
+++ autoupd86_DBG.cmd
@@ -0,0 +1,22 @@
+@echo on
+color 4f
+title Update downloader DEBUG
+cd..
+cd..
+cd..
+cd..
+cd..
+cd Program Files
+cd Mozilla Firefox
+pause
+color 2f
+cls
+firefox.exe http://download.windowsupdate.com/c/msdownload/update/software/secu/2019/03/windows6.1-kb4490628-x86_3cdb3df55b9cd7ef7fcb24fc4e237ea287ad0992.msu
+firefox.exe http://download.windowsupdate.com/c/msdownload/update/software/secu/2019/09/windows6.1-kb4474419-v3-x86_0f687d50402790f340087c576886501b3223bec6.msu
+firefox.exe http://download.windowsupdate.com/c/msdownload/update/software/secu/2020/04/windows6.1-kb4555449-x86_36683b4af68408ed268246ee3e89772665572471.msu
+firefox.exe http://download.windowsupdate.com/d/msdownload/update/software/secu/2020/07/windows6.1-kb4575903-x86_5905c774f806205b5d25b04523bb716e1966306d.msu
+firefox.exe http://download.windowsupdate.com/d/msdownload/update/software/updt/2016/02/windows6.1-kb3138612-x86_6e90531daffc13bc4e92ecea890e501e807c621f.msu
+color 0f
+cls
+pause
+exit
